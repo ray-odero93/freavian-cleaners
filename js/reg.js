6ad6694c5d8f31ec7374c610130b9b1cde18582f
Tito-74 <kipkirui133@gmail.com>
2021-08-04 14:16:14 +0300

    adding js for hiding errors

--- js/reg.js
+++ js/reg.js
@@ -1,3 +1,32 @@
-$(document).ready(function(){
-    console.log('Hey want to be strong programmer')
+$(function(){
+    $("#fname_error_message").hide();
+    $("#sname_error_message").hide();
+    $("#email_error_message").hide();
+    $("#password_error_message").hide();
+    $("#retype_password_error_message").hide();
+
+    var error_fname = false;
+    var error_sname = false;
+    var error_email = false;
+    var error_password = false;
+    var error_retype_password = false;
+
+    $("#form_fname").focusout(function(){
+        check_fname();
+    });
+    $("#form_sname").focusout(function(){
+        check_sname();
+    });
+    $("#form_email").focusout(function(){
+        check_email();
+    });
+    $("#form_password").focusout(function(){
+        check_password();
+    });
+    $("#form_retype_password").focusout(function(){
+        check_retype_password();
+    });
+
+   
+
 });
