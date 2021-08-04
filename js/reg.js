4350669fb75cafe33aaf0f2946deb927c842f6e2
Tito-74 <kipkirui133@gmail.com>
2021-08-04 14:19:41 +0300

    validating first and second names

--- js/reg.js
+++ js/reg.js
@@ -27,6 +27,37 @@ $(function(){
         check_retype_password();
     });
 
+    function check_fname(){
+        var pattern = /^[a-zA-Z]*$/;
+        var fname = $("#form_fname").val();
+        if (pattern.test(fname) && fname !== ''){
+            $("#fname_error_message").hide();
+            $("#form_fname").css("border-bottom","2px solid  #34F458");
+        } else{
+            $("#fname_error_message").html("Should contain Characters");
+            $("#fname_error_message").show();
+            $("#form_fname").css("border-bottom","2px solid  #F90A0A");
+            error_fname = true;
+
+        }
+    
+    }
+    function check_sname(){
+        var pattern = /^[a-zA-Z]*$/;
+        var fname = $("#form_sname").val();
+        if (pattern.test(fname) && fname !== ''){
+            $("#sname_error_message").hide();
+            $("#form_sname").css("border-bottom","2px solid  #34F458");
+        } else{
+            $("#sname_error_message").html("Should contain Characters");
+            $("#sname_error_message").show();
+            $("#form_sname").css("border-bottom","2px solid  #F90A0A");
+            error_sname = true;
+
+        }
+    
+    }
+
    
 
 });
