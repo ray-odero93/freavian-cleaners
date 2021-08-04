bbed0e396b54d1cce87b33fce8a5a5358cf1efd9
Parikirui <isaac.kirui@student.moringaschool.com>
2021-08-04 08:35:56 +0300

    changed the background color of the button to be cornflowerblue

--- js/index.js
+++ js/index.js
@@ -1,12 +1,23 @@
 function Login(){
-var username=document.getElementById("Uname").value;
-var userpassword=document.getElementById("Pass").value;
-if(username=="admin" && password=="123")
+var username = "admin";
+var password = "123";
+
+var username=document.getElementById("Uname");
+var password=document.getElementById("Pass");
+if(username.value == "admin" && password.value == "123")
 {
     window.location.replace("mainpage.html");
+    // alert("correct details");
+    // return false;
 }
 else{
     alert("Wrong details");
 }
+if(username.value == "" || password.value == "")
+{
+    alert("fill the blank details");
 }
+}
+
+
 
