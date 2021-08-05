f353d2aca2b5d6253008bdd7a1c78b2bc8f0a434
Parikirui <isaac.kirui@student.moringaschool.com>
2021-08-05 08:08:16 +0300

    worked on to improve the control flow section

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 function Login(){
 var username = "admin";
-var password = "admin1234";
+var password = "123";
 
 var username=document.getElementById("Uname");
 var password=document.getElementById("Pass");
@@ -10,19 +10,16 @@ if(username.value == "admin" && password.value == "admin1234")
     // alert("correct details");
     // return false;
 }
-// else{
-//     alert("Wrong Username or Password");
-// }
-if(username.value == "" || password.value == "")
-{
-    alert("fill the blank details");
-    return false;
-}
 else{
     alert("Wrong Username or Password");
     return false;
 }
 }
+// if(username.value == "" || password.value == "")
+// {
+//     alert("fill the blank details");
+// }
+
 
 
 
